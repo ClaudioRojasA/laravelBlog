6a606e4d672413d836b483902cd79f227d276feb
ClaudioRojasA <rojasamado.claudio@gmail.com>
2023-12-25 15:58:25 -0300

    Add image to post.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,15 +22,38 @@ class PostController extends Controller
 
     public function store(Request $request){
 
+        $image_image = "";
+        if($request->hasFile('image')){
+            $image = $request->file('image');
+            $image_image = $image->getClientOriginalName();
+        }
         $post = new Post();
         $post->post_title = $request->post_title;
         $post->category_id = $request->category_id;
-        //$post->image = $request->image;
+
+        $post->image = $image_image;
         $post->post_content = $request->post_content;
 
         $post->save();
 
+        $this->uploadImage($request, $post->id);
+
         return redirect()->back()->with('message','Post Added Successfully');
+    }
+
+    public function uploadImage(Request $request, $post_id){
+
+        if($request->hasFile('image')){
+            $path = public_path("post_images/post_$post_id");
+
+            if(! file_exists($path)){
+                mkdir($path, 0777, true);
+            }
+            $image = $request->file('image');
+            $image_image = $image->getClientOriginalName();
+
+            $image->move($path, $image_image);
+        }
     }
 
 }
